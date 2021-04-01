c04250f5b21d2e2dd21d2e66870f41a8e5e0abd7
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2021-04-01 09:56:31 -0700

    Input class implementation

---
+++ XenoEngine/Code/Core/Input.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "Core.h"
+
+#include <GLM/vec2.hpp>
+#include <SDL2/SDL.h>
+
+namespace Xeno
+{
+	// @brief Class to abstract SDL inputs.
+	class XENO_API Input
+	{
+	public:
+		~Input();
+
+		enum class AxisType
+		{
+			MOUSE,
+			JOYSTICK
+		};
+
+		// @brief Gets the key state.
+		// @return bool: True if key is pressed, else false.
+		static bool GetKey(SDL_Scancode code);
+		// @brief Returns true during the frame the user starts pressing down the key.
+		// @return bool: True if key is pressed, else false.
+		static bool GetKeyDown(SDL_Scancode code);
+		// @brief Returns true during the frame the user releases the key.
+		// @return bool: True if key is released, else false.
+		static bool GetKeyUp(SDL_Scancode code);
+		// @brief Gets the current mouse mPosition on the screen.
+		// @return glm::vec2: The mouse x and y mPosition.
+		static glm::vec2 GetAxis(AxisType type);
+
+	private:
+		// @brief Default constructor.
+		Input();
+
+		// @brief Updates keyboard status.
+		void Update();
+		void ProcessEvents(const SDL_Event& event);
+
+		// @brief Stores the current keyboard state.
+		static inline const Uint8* mKeyboardState;
+		// @brief Stores the last keyboard state.
+		static inline Uint8* mPrevKeyboardState;
+		// @brief Number of valid keys.
+		static inline int mNumKeys;
+		// @brief Current mouse state.
+		static inline Uint32 mMouseState;
+		// @brief Previous mouse state.
+		static inline Uint32 mPrevMouseState;
+		// @brief Stores the value of the mouse axis.
+		static inline glm::vec2 mMouseAxis;
+		// @brief Stores the value of the joystick axis.
+		static inline glm::vec2 mJoystickAxis;
+
+		friend class Application;
+	};
+}
