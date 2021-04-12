bee2ed9c8308ed918f9d97c40a8b0757077f2e1b
benjaminmao123 <43651347+benjaminmao123@users.noreply.github.com>
2021-04-12 03:09:39 -0700

    Update Sandbox.h

--- Sandbox/Code/Sandbox.h
+++ Sandbox/Code/Sandbox.h
@@ -19,28 +19,10 @@ protected:
         ResourceManager::AddTexture(std::make_shared<Texture>("Assets/Textures/container.jpg"));
 
         SceneManager::AddScene("sandbox");
-        Scene* scene = SceneManager::GetScene("sandbox");
+        mScene = SceneManager::GetScene("sandbox");
 
-        Entity* cameraEntity = scene->CreateEntity("camera");
-        mCameraComponent = cameraEntity->AddComponent<Camera>();
-        mCameraController = cameraEntity->AddComponent<SceneCameraController>();
-        mCameraComponent->SetProjectionType(Camera::ProjectionType::PERSPECTIVE);
-        scene->SetMainCamera(mCameraComponent);
-
-        Entity* boxEntity = scene->CreateEntity("box");
-        Renderer* boxRenderer = boxEntity->AddComponent<MeshRenderer>();
-        boxRenderer->GetMaterial().SetTexture(ResourceManager::GetTextureRef("Assets/Textures/container.jpg"));
-
-        const auto cubeMesh = std::make_shared<Cube>();
-        auto* meshFilter = boxEntity->GetComponent<MeshFilter>();
-        meshFilter->SetMesh(cubeMesh);
-
-        //Entity* boxEntity2 = scene->CreateEntity("box2");
-        //Renderer* boxRenderer2 = boxEntity2->AddComponent<SpriteRenderer>();
-        //boxRenderer2->GetMaterial().SetTexture(ResourceManager::GetTextureRef("Assets/Textures/container.jpg"));
-        //boxEntity2->GetTransform().SetScale(100, 100, 1);
-        //boxEntity2->GetTransform().SetPosition(Window::GetCenter().x, Window::GetCenter().y, 0);
-        //mCameraController->SetPanSpeed(1000.0f);
+        TestOrthographic();
+        //TestPerspective();
     }
 
     void OnUpdate() override
@@ -49,6 +31,40 @@ protected:
     }
 
 private:
+    void TestOrthographic()
+    {
+        Entity* cameraEntity = mScene->CreateEntity("camera");
+        mCameraComponent = cameraEntity->AddComponent<Camera>();
+        mCameraController = cameraEntity->AddComponent<SceneCameraController>();
+        mCameraComponent->SetProjectionType(Camera::ProjectionType::ORTHOGRAPHIC);
+        mScene->SetMainCamera(mCameraComponent);
+
+        Entity* boxEntity = mScene->CreateEntity("box");
+        Renderer* boxRenderer = boxEntity->AddComponent<SpriteRenderer>();
+        boxRenderer->GetMaterial().SetTexture(ResourceManager::GetTextureRef("Assets/Textures/container.jpg"));
+        boxEntity->GetTransform().SetScale(100, 100, 1);
+        boxEntity->GetTransform().SetPosition(Window::GetCenter().x, Window::GetCenter().y, 0);
+        mCameraController->SetPanSpeed(1000.0f);
+    }
+
+    void TestPerspective()
+    {
+        Entity* cameraEntity = mScene->CreateEntity("camera");
+        mCameraComponent = cameraEntity->AddComponent<Camera>();
+        mCameraController = cameraEntity->AddComponent<SceneCameraController>();
+        mCameraComponent->SetProjectionType(Camera::ProjectionType::PERSPECTIVE);
+        mScene->SetMainCamera(mCameraComponent);
+
+        Entity* boxEntity = mScene->CreateEntity("box");
+        Renderer* boxRenderer = boxEntity->AddComponent<MeshRenderer>();
+        boxRenderer->GetMaterial().SetTexture(ResourceManager::GetTextureRef("Assets/Textures/container.jpg"));
+
+        const auto cubeMesh = std::make_shared<Cube>();
+        auto* meshFilter = boxEntity->GetComponent<MeshFilter>();
+        meshFilter->SetMesh(cubeMesh);
+    }
+
     Camera* mCameraComponent = nullptr;
     SceneCameraController* mCameraController = nullptr;
+    Scene* mScene = nullptr;
 };
